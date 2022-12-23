016931c23405d50165579718d2967e4505b698c6
Petar Belokonski <petar.belokonski@tumba.solutions>
2022-12-23 19:36:57 +0200

    Move ChessBoard and ChessBoardViewController to main target

--- AlternativeChess/ViewController.swift
+++ AlternativeChess/ViewController.swift
@@ -7,6 +7,48 @@
 
 import UIKit
 
+class ChessBoardViewController: UIViewController {
+    
+    // Aproach 1 - where UIVieController's view is of our type
+//    override func loadView() {
+//        view = ChessBoardView()
+//    }
+
+    // Approach 2 - where UIVieController's view subview is of our type
+    override func viewDidLoad() {
+        let chessBoardView = ChessBoardView()
+        view.addSubview(chessBoardView)
+    }
+}
+
+struct Square {
+    enum Color {
+        case white
+        case black
+    }
+    let color: Color
+}
+
+class ChessBoardView: UIView {
+    private static let squaresInRow = 8
+
+    let squares: [[Square]] = {
+        var board: [[Square]] = []
+        for row in 0 ..< ChessBoardView.squaresInRow {
+            board.append([])
+            for column in 0 ..< ChessBoardView.squaresInRow {
+                let squareColor: Square.Color = (row + column).isMultiple(of: 2) ? .black : .white
+                board[row].append(Square(color: squareColor))
+            }
+        }
+        return board
+    }()
+
+    override func draw(_ rect: CGRect) {
+        // TODO: draw squares in rect
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
--- AlternativeChessTests/AlternativeChessTests.swift
+++ AlternativeChessTests/AlternativeChessTests.swift
@@ -8,48 +8,6 @@
 import XCTest
 @testable import AlternativeChess
 
-class ChessBoardViewController: UIViewController {
-    
-    // Aproach 1 - where UIVieController's view is of our type
-//    override func loadView() {
-//        view = ChessBoardView()
-//    }
-
-    // Approach 2 - where UIVieController's view subview is of our type
-    override func viewDidLoad() {
-        let chessBoardView = ChessBoardView()
-        view.addSubview(chessBoardView)
-    }
-}
-
-struct Square {
-    enum Color {
-        case white
-        case black
-    }
-    let color: Color
-}
-
-class ChessBoardView: UIView {
-    private static let squaresInRow = 8
-
-    let squares: [[Square]] = {
-        var board: [[Square]] = []
-        for row in 0 ..< ChessBoardView.squaresInRow {
-            board.append([])
-            for column in 0 ..< ChessBoardView.squaresInRow {
-                let squareColor: Square.Color = (row + column).isMultiple(of: 2) ? .black : .white
-                board[row].append(Square(color: squareColor))
-            }
-        }
-        return board
-    }()
-
-    override func draw(_ rect: CGRect) {
-        // TODO: draw squares in rect
-    }
-}
-
 final class AlternativeChessTests: XCTestCase {
     func test_ChessBoardViewControllerContainsChessView() {
         // Given the app is launched
