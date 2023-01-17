cce86db74ce605d52f4c7a364e06a2409558c098
emo <emiliyan2003e@gmail.com>
2023-01-17 11:19:17 +0200

    Initial board structure implementation.

---
+++ AlternativeChess/ChessBoard.swift
@@ -0,0 +1,32 @@
+//
+//  ChessBoard.swift
+//  AlternativeChess
+//
+//  Created by emo on 15.01.23.
+//
+
+import Foundation
+
+struct Square {
+    enum Color {
+        case white
+        case black
+    }
+    let color: Color
+    var sideOfSquare = 0.0
+}
+
+struct Board {
+    
+    var squares: [[Square]] = {
+        var board: [[Square]] = []
+        for row in 0 ..< ChessBoardView.squaresInRow {
+            board.append([])
+            for column in 0 ..< ChessBoardView.squaresInRow {
+                let squareColor: Square.Color = (row + column).isMultiple(of: 2) ? .white : .black
+                board[row].append(Square(color: squareColor))
+            }
+        }
+        return board
+    }()
+}
