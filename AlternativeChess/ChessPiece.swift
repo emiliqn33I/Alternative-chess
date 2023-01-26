a151ace9191bfd049bac1d1f73432bc63a7f6168
emo <emiliyan2003e@gmail.com>
2023-01-26 15:43:55 +0200

    Implemented structure for position.

--- AlternativeChess/ChessPiece.swift
+++ AlternativeChess/ChessPiece.swift
@@ -7,98 +7,81 @@
 
 import Foundation
 
-struct IntWithString: RawRepresentable, Codable, ExpressibleByStringLiteral, Equatable {
-    typealias StringLiteralType = String
-
-    let value: Int
-    let name: String
-
-    var rawValue: String {
-        return "\(value),\(name)"
+struct Position {
+    enum Rank: Int, CaseIterable {
+        case first
+        case second
+        case third
+        case fourth
+        case fifth
+        case sixth
+        case seventh
+        case eighth
     }
-    init?(rawValue: String) {
-        let components = rawValue.components(separatedBy: ",")
-        guard components.count == 2 else {
-            return nil
+    enum File: Int , CaseIterable{
+        case A
+        case B
+        case C
+        case D
+        case E
+        case F
+        case G
+        case H
+    }
+    var file: File?
+    var rank: Rank?
+    init(file: File?, rank: Rank?) {
+        self.file = file
+        self.rank = rank
+    }
+    mutating func canChnageFile(delta: Int) -> Piece? {
+        var chessEngine = ChessEngine()
+        chessEngine.initialiseGame()
+        let pieces = chessEngine.pieces
+        if ((delta + (file?.rawValue ?? 100) >= 0) && (delta + (file?.rawValue ?? 100) <= 7)) {
+            let result = pieces.filter( { $0.position.file?.rawValue ?? 1000 == (delta + (file?.rawValue ?? 100)) } )
+            if (result.isEmpty) {
+                return nil
+            }
+            return result[0]
         }
-        self.value = Int(components[0])!
-        self.name = components[1]
+        return nil
     }
-
-    init(stringLiteral rawValue: String) {
-        self.init(rawValue: rawValue)!
+    mutating func canChnageRank(delta: Int) -> Piece? {
+        var chessEngine = ChessEngine()
+        chessEngine.initialiseGame()
+        let pieces = chessEngine.pieces
+        if ((delta + (rank?.rawValue ?? 100) >= 0) && (delta + (rank?.rawValue ?? 100) <= 7)) {
+            let result = pieces.filter( { $0.position.rank?.rawValue ?? 1000 == (delta + (rank?.rawValue ?? 100))} )
+            if (result.isEmpty) {
+                return nil
+            }
+            return result[0]
+        }
+        return nil
     }
 }
 
-struct IntWithInt: RawRepresentable, Codable, ExpressibleByStringLiteral, Equatable {
-    typealias StringLiteralType = String
-
-    let value: Int
-    let name: Int
-
-    var rawValue: String {
-        return "\(value),\(name)"
+struct Piece: Equatable {
+    static func == (lhs: Piece, rhs: Piece) -> Bool {
+        return lhs.position.file == rhs.position.file && lhs.position.rank == rhs.position.rank
     }
-    init?(rawValue: String) {
-        let components = rawValue.components(separatedBy: ",")
-        guard components.count == 2 else {
-            return nil
-        }
-        self.value = Int(components[0])!
-        self.name = Int(components[1])!
-    }
-
-    init(stringLiteral rawValue: String) {
-        self.init(rawValue: rawValue)!
-    }
-}
-
-struct Piece {
-    enum Rank: IntWithInt {
-        case first = "7,1" // One value communicates with the user(1) and the other with the array of pieces(7),
-        case second = "6,2"
-        case third = "5,3"
-        case fourth = "4,4"
-        case fifth = "3,5"
-        case sixth = "2,6"
-        case seventh = "1,7"
-        case eighth = "0,8"
-        
-        var value: Int { return rawValue.value }
-        var name: Int { return rawValue.name }
-    }
-    enum File: IntWithString {
-        case A = "0,A" // One value communicates with the user(A) and the other with the array of pieces(0),
-        case B = "1,B"
-        case C = "2,C"
-        case D = "3,D"
-        case E = "4,E"
-        case F = "5,F"
-        case G = "6,G"
-        case H = "7,H"
-        
-        var value: Int { return rawValue.value }
-        var name: String { return rawValue.name }
-    }
+    
+    
     enum PieceType {
         case pawn
-        case rook
-        case none // Case for square without a piece. Needed for initialising empty square.
     }
     enum Color {
         case white
         case black
-        case none // Case for square without a piece. Needed for initialising empty square.
     }
-    var file: File
-    var rank: Rank
+    var position: Position
     var type: PieceType
     var colour: Color
     
-    init(type: PieceType, colour: Color, file: File, rank: Rank) {
+    init(type: PieceType, colour: Color, position: Position) {
         self.type = type
         self.colour = colour
-        self.file = file
-        self.rank = rank
+        self.position = position
     }
 }
