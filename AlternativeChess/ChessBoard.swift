cf03feaa7f9566bc6d43024fe09e2af50a44370d
emo <emiliyan2003e@gmail.com>
2023-01-26 15:38:34 +0200

    Implemented function which puts pieces on the on squares, based on the coordinates of the piece.

--- AlternativeChess/ChessBoard.swift
+++ AlternativeChess/ChessBoard.swift
@@ -14,6 +14,7 @@ struct Square {
     }
     let color: Color
     var sideOfSquare = 0.0
+    var piece: Piece?
 }
 
 struct Board {
@@ -24,9 +25,24 @@ struct Board {
             board.append([])
             for column in 0 ..< ChessBoardView.squaresInRow {
                 let squareColor: Square.Color = (row + column).isMultiple(of: 2) ? .white : .black
-                board[row].append(Square(color: squareColor))
+                board[row].append(Square(color: squareColor, piece: nil))
             }
         }
         return board
     }()
+    
+    mutating func renewBoard() {
+        var chessEngine = ChessEngine()
+        chessEngine.initialiseGame()
+        let pieces = chessEngine.pieces
+        for row in 0...7 {
+            for flank in 0...7 {
+                for i in pieces {
+                    if((row == i.position.rank?.rawValue) && (flank == i.position.file?.rawValue)) {
+                        squares[row][flank].piece = i
+                    }
+                }
+            }
+        }
+    }
 }
