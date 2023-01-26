a7f302d1576a94e79b41218cdcecfba6a102a03c
emo <emiliyan2003e@gmail.com>
2023-01-26 16:25:07 +0200

    Implemented logic for a white pawn to move once

--- AlternativeChess/ChessEngine.swift
+++ AlternativeChess/ChessEngine.swift
@@ -11,7 +11,8 @@ struct ChessEngine {
     var pieces = [Piece]()
     mutating func initialiseGame() {
         pieces = [
-            Piece(type: .pawn, colour: .white, position: Position(file: .A, rank: .second))
+            Piece(type: .pawn, colour: .white, position: Position(file: .A, rank: .second)),
+            Piece(type: .pawn, colour: .white, position: Position(file: .B, rank: .third))
         ]
         
     }
@@ -33,12 +34,39 @@ struct ChessEngine {
         
         if (pawnIsWhite) {
             let CheckWPawnOn2Rank = (pawn.position.rank == .second)
-            let CheckWPawnMove1Square = (pawn.position.canChnageRank(delta: -1) == nil)
-            let CheckWPawnMove2Squares = (pawn.position.canChnageRank(delta: -2) == nil)
+            let CheckWPawnMove1Square = (pawn.position.canChnageRank(delta: 1) == nil)
+            let CheckWPawnMove2Squares = (pawn.position.canChnageRank(delta: 2) == nil)
             if (CheckWPawnOn2Rank && CheckWPawnMove1Square && CheckWPawnMove2Squares) {
                 for i in 1...2 {
                     for aRank in Position.Rank.allCases {
                         if (aRank.rawValue == ((pawn.position.rank?.rawValue ?? 100 ) + i)) {
+                            let position = Position(file: pawn.position.file, rank: aRank)
+                            coordinates.append(position)
+                            break
+                        }
+                    }
+                }
+            }
+            let CheckWPNot8Rank = (pawn.position.rank != .eighth)
+            if (CheckWPNot8Rank && CheckWPawnMove1Square) {
+                var flag = false
+                for i in coordinates {
+                    let CompareFilesIfAlreadyExisted = (i.file == pawn.position.file)
+                    var currentRank = i.rank
+                    for aRank in Position.Rank.allCases {
+                        if (aRank.rawValue == ((i.rank?.rawValue ?? 100 ) + 1)) {
+                            currentRank = aRank
+                            break
+                        }
+                    }
+                    let CompareRanksIfAlreadyExisted = (currentRank != i.rank)
+                    if (CompareFilesIfAlreadyExisted && CompareRanksIfAlreadyExisted) {
+                        flag = true
+                    }
+                }
+                if (flag == false) {
+                    for aRank in Position.Rank.allCases {
+                        if (aRank.rawValue == ((pawn.position.rank?.rawValue ?? 100 ) + 1)) {
                             let position = Position(file: pawn.position.file, rank: aRank)
                             coordinates.append(position)
                             break
