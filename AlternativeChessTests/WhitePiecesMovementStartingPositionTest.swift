8721272a71e13db70a17370edf656367c11b7498
emo <emiliyan2003e@gmail.com>
2023-02-01 15:26:35 +0200

    Implementing tests for every piece at it starting position for white.

--- AlternativeChessTests/WhitePiecesMovementStartingPositionTest.swift
+++ AlternativeChessTests/WhitePiecesMovementStartingPositionTest.swift
@@ -40,32 +40,73 @@ final class WhitePiecesMovementStartingPositionTest: XCTestCase {
         let chessEngine = createSUT(pieces: pieces)
         // When they tap the a piece
         // Then the A4, A3, B4, B3, C3, C4, D3, D4, E3, E4, F3, F4, G3, G4, H3, H4 square on the board will be highlighted
-        var validMovesWhitePieces = [Position]()
+        var validMovesA2pawn = chessEngine.possibleMoves(piece: pieces[0])
+        XCTAssertTrue(validMovesA2pawn.count == 2)
+        XCTAssertTrue((validMovesA2pawn[0].file == .A) && (validMovesA2pawn[0].rank == .third))
+        XCTAssertTrue((validMovesA2pawn[1].file == .A) && (validMovesA2pawn[1].rank == .fourth))
         
-        for piece in pieces {
-            validMovesWhitePieces += chessEngine.possibleMoves(piece: piece)
-        }
+        var validMovesB2pawn = chessEngine.possibleMoves(piece: pieces[1])
+        XCTAssertTrue(validMovesB2pawn.count == 2)
+        XCTAssertTrue((validMovesB2pawn[0].file == .B) && (validMovesB2pawn[0].rank == .third))
+        XCTAssertTrue((validMovesB2pawn[1].file == .B) && (validMovesB2pawn[1].rank == .fourth))
+        
+        var validMovesC2pawn = chessEngine.possibleMoves(piece: pieces[2])
+        XCTAssertTrue(validMovesC2pawn.count == 2)
+        XCTAssertTrue((validMovesC2pawn[0].file == .C) && (validMovesC2pawn[0].rank == .third))
+        XCTAssertTrue((validMovesC2pawn[1].file == .C) && (validMovesC2pawn[1].rank == .fourth))
+        
+        var validMovesD2pawn = chessEngine.possibleMoves(piece: pieces[3])
+        XCTAssertTrue(validMovesD2pawn.count == 2)
+        XCTAssertTrue((validMovesD2pawn[0].file == .D) && (validMovesD2pawn[0].rank == .third))
+        XCTAssertTrue((validMovesD2pawn[1].file == .D) && (validMovesD2pawn[1].rank == .fourth))
+        
+        var validMovesE2pawn = chessEngine.possibleMoves(piece: pieces[4])
+        XCTAssertTrue(validMovesE2pawn.count == 2)
+        XCTAssertTrue((validMovesE2pawn[0].file == .E) && (validMovesE2pawn[0].rank == .third))
+        XCTAssertTrue((validMovesE2pawn[1].file == .E) && (validMovesE2pawn[1].rank == .fourth))
+        
+        var validMovesF2pawn = chessEngine.possibleMoves(piece: pieces[5])
+        XCTAssertTrue(validMovesF2pawn.count == 2)
+        XCTAssertTrue((validMovesF2pawn[0].file == .F) && (validMovesF2pawn[0].rank == .third))
+        XCTAssertTrue((validMovesF2pawn[1].file == .F) && (validMovesF2pawn[1].rank == .fourth))
+        
+        var validMovesG2pawn = chessEngine.possibleMoves(piece: pieces[6])
+        XCTAssertTrue(validMovesG2pawn.count == 2)
+        XCTAssertTrue((validMovesG2pawn[0].file == .G) && (validMovesG2pawn[0].rank == .third))
+        XCTAssertTrue((validMovesG2pawn[1].file == .G) && (validMovesG2pawn[1].rank == .fourth))
+        
+        var validMovesH2pawn = chessEngine.possibleMoves(piece: pieces[7])
+        XCTAssertTrue(validMovesH2pawn.count == 2)
+        XCTAssertTrue((validMovesH2pawn[0].file == .H) && (validMovesH2pawn[0].rank == .third))
+        XCTAssertTrue((validMovesH2pawn[1].file == .H) && (validMovesH2pawn[1].rank == .fourth))
+        
+        var validMovesA1Rook = chessEngine.possibleMoves(piece: pieces[8])
+        XCTAssertTrue(validMovesA1Rook.isEmpty)
+        
+        var validMovesB1Knight = chessEngine.possibleMoves(piece: pieces[9])
+        XCTAssertTrue(validMovesB1Knight.count == 2)
+        XCTAssertTrue((validMovesB1Knight[0].file == .A) && (validMovesB1Knight[0].rank == .third))
+        XCTAssertTrue((validMovesB1Knight[1].file == .C) && (validMovesB1Knight[1].rank == .third))
+        
+        var validMovesC1Bishop = chessEngine.possibleMoves(piece: pieces[10])
+        XCTAssertTrue(validMovesC1Bishop.isEmpty)
+        
+        var validMovesD1Queen = chessEngine.possibleMoves(piece: pieces[11])
+        XCTAssertTrue(validMovesD1Queen.isEmpty)
+        
+        var validMovesE1King = chessEngine.possibleMoves(piece: pieces[12])
+        XCTAssertTrue(validMovesE1King.isEmpty)
+        
+        var validMovesF1Bishop = chessEngine.possibleMoves(piece: pieces[13])
+        XCTAssertTrue(validMovesF1Bishop.isEmpty)
+        
+        var validMovesG1Knight = chessEngine.possibleMoves(piece: pieces[14])
+        XCTAssertTrue(validMovesG1Knight.count == 2)
+        XCTAssertTrue((validMovesG1Knight[0].file == .F) && (validMovesG1Knight[0].rank == .third))
+        XCTAssertTrue((validMovesG1Knight[1].file == .H) && (validMovesG1Knight[1].rank == .third))
+        
+        var validMovesH1Rook = chessEngine.possibleMoves(piece: pieces[15])
+        XCTAssertTrue(validMovesH1Rook.isEmpty)
 
-        XCTAssert(validMovesWhitePieces.count == 20)
-        XCTAssert((validMovesWhitePieces[0].file == .A) && (validMovesWhitePieces[0].rank == .third))
-        XCTAssert((validMovesWhitePieces[1].file == .A) && (validMovesWhitePieces[1].rank == .fourth))
-        XCTAssert((validMovesWhitePieces[2].file == .B) && (validMovesWhitePieces[2].rank == .third))
-        XCTAssert((validMovesWhitePieces[3].file == .B) && (validMovesWhitePieces[3].rank == .fourth))
-        XCTAssert((validMovesWhitePieces[4].file == .C) && (validMovesWhitePieces[4].rank == .third))
-        XCTAssert((validMovesWhitePieces[5].file == .C) && (validMovesWhitePieces[5].rank == .fourth))
-        XCTAssert((validMovesWhitePieces[6].file == .D) && (validMovesWhitePieces[6].rank == .third))
-        XCTAssert((validMovesWhitePieces[7].file == .D) && (validMovesWhitePieces[7].rank == .fourth))
-        XCTAssert((validMovesWhitePieces[8].file == .E) && (validMovesWhitePieces[8].rank == .third))
-        XCTAssert((validMovesWhitePieces[9].file == .E) && (validMovesWhitePieces[9].rank == .fourth))
-        XCTAssert((validMovesWhitePieces[10].file == .F) && (validMovesWhitePieces[10].rank == .third))
-        XCTAssert((validMovesWhitePieces[11].file == .F) && (validMovesWhitePieces[11].rank == .fourth))
-        XCTAssert((validMovesWhitePieces[12].file == .G) && (validMovesWhitePieces[12].rank == .third))
-        XCTAssert((validMovesWhitePieces[13].file == .G) && (validMovesWhitePieces[13].rank == .fourth))
-        XCTAssert((validMovesWhitePieces[14].file == .H) && (validMovesWhitePieces[14].rank == .third))
-        XCTAssert((validMovesWhitePieces[15].file == .H) && (validMovesWhitePieces[15].rank == .fourth))
-        XCTAssert((validMovesWhitePieces[16].file == .A) && (validMovesWhitePieces[16].rank == .third))
-        XCTAssert((validMovesWhitePieces[17].file == .C) && (validMovesWhitePieces[17].rank == .third))
-        XCTAssert((validMovesWhitePieces[18].file == .F) && (validMovesWhitePieces[18].rank == .third))
-        XCTAssert((validMovesWhitePieces[19].file == .H) && (validMovesWhitePieces[19].rank == .third))
     }
 }
