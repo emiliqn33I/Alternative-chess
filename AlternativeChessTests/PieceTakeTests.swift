d5515ea8aab3c20f2016b4e49eca15797970e850
emo <emiliyan2003e@gmail.com>
2023-02-02 13:51:56 +0200

    Implementing test that checks is white rook can take pieces.

--- AlternativeChessTests/PieceTakeTests.swift
+++ AlternativeChessTests/PieceTakeTests.swift
@@ -28,4 +28,23 @@ final class PieceTakeTests: XCTestCase {
         XCTAssertTrue(validMovesB2pawn.contains(Position(file: .C, rank: .third)))
         XCTAssertTrue(validMovesB2pawn.contains(Position(file: .A, rank: .third)))
     }
+    
+    func testWhiteRookTakes() {
+        // Given the user is on the board screen
+        // And there is white rook at D4 and black pawns at B4, D6, D2 and F4
+        let pieces = [Piece(type: .pawn, colour: .black, position: Position(file: .D, rank: .sixth)),
+                      Piece(type: .pawn, colour: .black, position: Position(file: .B, rank: .fourth)),
+                      Piece(type: .rook, colour: .white, position: Position(file: .D, rank: .fourth)),
+                      Piece(type: .pawn, colour: .black, position: Position(file: .D, rank: .second)),
+                      Piece(type: .pawn, colour: .black, position: Position(file: .F, rank: .fourth))]
+        let chessEngine = createSUT(pieces: pieces)
+        // When they tap the D4 rook
+        // Then the B4, D6, D2 and F4 will be highlighted
+        let validMovesB2pawn = chessEngine.possibleMoves(piece: pieces[2])
+        XCTAssertTrue(validMovesB2pawn.count == 8)
+        XCTAssertTrue(validMovesB2pawn.contains(Position(file: .D, rank: .sixth)))
+        XCTAssertTrue(validMovesB2pawn.contains(Position(file: .B, rank: .fourth)))
+        XCTAssertTrue(validMovesB2pawn.contains(Position(file: .D, rank: .second)))
+        XCTAssertTrue(validMovesB2pawn.contains(Position(file: .F, rank: .fourth)))
+    }
 }
