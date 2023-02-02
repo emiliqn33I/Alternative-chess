b099e4799db7b4f98b5b054fbe065cb6c9b13337
emo <emiliyan2003e@gmail.com>
2023-02-02 13:58:22 +0200

    Implementing test that checks if white bishop can take pieces.

--- AlternativeChessTests/PieceTakeTests.swift
+++ AlternativeChessTests/PieceTakeTests.swift
@@ -40,11 +40,30 @@ final class PieceTakeTests: XCTestCase {
         let chessEngine = createSUT(pieces: pieces)
         // When they tap the D4 rook
         // Then the B4, D6, D2 and F4 will be highlighted
-        let validMovesB2pawn = chessEngine.possibleMoves(piece: pieces[2])
-        XCTAssertTrue(validMovesB2pawn.count == 8)
-        XCTAssertTrue(validMovesB2pawn.contains(Position(file: .D, rank: .sixth)))
-        XCTAssertTrue(validMovesB2pawn.contains(Position(file: .B, rank: .fourth)))
-        XCTAssertTrue(validMovesB2pawn.contains(Position(file: .D, rank: .second)))
-        XCTAssertTrue(validMovesB2pawn.contains(Position(file: .F, rank: .fourth)))
+        let validMovesD4Rook = chessEngine.possibleMoves(piece: pieces[2])
+        XCTAssertTrue(validMovesD4Rook.count == 8)
+        XCTAssertTrue(validMovesD4Rook.contains(Position(file: .D, rank: .sixth)))
+        XCTAssertTrue(validMovesD4Rook.contains(Position(file: .B, rank: .fourth)))
+        XCTAssertTrue(validMovesD4Rook.contains(Position(file: .D, rank: .second)))
+        XCTAssertTrue(validMovesD4Rook.contains(Position(file: .F, rank: .fourth)))
+    }
+    
+    func testWhiteBishopTakes() {
+        // Given the user is on the board screen
+        // And there is white rook at D4 and black pawns at B4, D6, D2 and F4
+        let pieces = [Piece(type: .pawn, colour: .black, position: Position(file: .B, rank: .sixth)),
+                      Piece(type: .pawn, colour: .black, position: Position(file: .B, rank: .second)),
+                      Piece(type: .bishop, colour: .white, position: Position(file: .D, rank: .fourth)),
+                      Piece(type: .pawn, colour: .black, position: Position(file: .F, rank: .second)),
+                      Piece(type: .pawn, colour: .black, position: Position(file: .F, rank: .sixth))]
+        let chessEngine = createSUT(pieces: pieces)
+        // When they tap the D4 bishop
+        // Then the B4, D6, D2 and F4 will be highlighted
+        let validMovesD4Bishop = chessEngine.possibleMoves(piece: pieces[2])
+        XCTAssertTrue(validMovesD4Bishop.count == 8)
+        XCTAssertTrue(validMovesD4Bishop.contains(Position(file: .B, rank: .sixth)))
+        XCTAssertTrue(validMovesD4Bishop.contains(Position(file: .B, rank: .second)))
+        XCTAssertTrue(validMovesD4Bishop.contains(Position(file: .F, rank: .second)))
+        XCTAssertTrue(validMovesD4Bishop.contains(Position(file: .F, rank: .sixth)))
     }
 }
