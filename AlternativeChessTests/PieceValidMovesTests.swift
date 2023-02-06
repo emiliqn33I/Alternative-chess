76fcd1e4ff3e11912012781cd21f9131cbaab414
emo <emiliyan2003e@gmail.com>
2023-02-06 21:46:33 +0200

    Implementing test for en Passant.

--- AlternativeChessTests/PieceValidMovesTests.swift
+++ AlternativeChessTests/PieceValidMovesTests.swift
@@ -26,4 +26,23 @@ final class PieceValidMovesTests: XCTestCase {
 
         XCTAssertTrue(validMoves.isEmpty)
     }
+    
+    func testPawnEnPassant() {
+        // Given the user has a white pawn at E5 and black pawn at F5
+        let pieces = [Piece(type: .pawn, colour: .white, position: Position(file: .E, rank: .fifth)),
+                      Piece(type: .pawn, colour: .black, position: Position(file: .F, rank: .fifth))]
+        let chessEngine = createSUT(pieces: pieces)
+        // And it's his turn to move
+        chessEngine.turn = true
+        // And the last move of the black pieces was a pawn at F7 to F5
+        if chessEngine.history.last != (Position(file: .F, rank: .seventh), Position(file: .F, rank: .fifth)) {
+            XCTFail()
+        }
+        // Then the user will be able to take the pawn at F5 while moveing to F6 with the white pawn
+        let validMoves = chessEngine.validMoves(for: pieces[0])
+        XCTAssertTrue(validMoves.contains(Position(file: .F, rank: .sixth)))
+        
+        chessEngine.place(piece: pieces[0], at: Position(file: .F, rank: .sixth))
+        XCTAssertTrue(pieces[0].position == Position(file: .F, rank: .sixth))
+    }
 }
