5c5b50c2e4d56f7b0cc51c54867ec89867112699
emo <emiliyan2003e@gmail.com>
2023-02-09 13:57:24 +0200

    Implementing test for rokado of black pieces.

--- AlternativeChessTests/PieceValidMovesTests.swift
+++ AlternativeChessTests/PieceValidMovesTests.swift
@@ -190,4 +190,32 @@ final class PieceValidMovesTests: XCTestCase {
         XCTAssertTrue(chessEngine.pieces.contains(Piece(type: .rook, colour: .white, position: Position(file: .D, rank: .first))))
         XCTAssertTrue(chessEngine.pieces.contains(Piece(type: .king, colour: .white, position: Position(file: .C, rank: .first))))
     }
+    
+    func testRokadoBlackKingSide() {
+        // Given the user has a black king at E8 and black rook at H8
+        let pieces = [Piece(type: .king, colour: .black, position: Position(file: .E, rank: .eighth)),
+                      Piece(type: .rook, colour: .black, position: Position(file: .H, rank: .eighth))]
+        let chessEngine = createSUT(pieces: pieces, turn: true)
+        // And there are no pieces that are attacking the king between the rook
+        // Then the white king can castle kingside and will have G8 as a valid move
+        XCTAssertTrue(chessEngine.validMoves(for: pieces[0]).contains(Position(file: .G, rank: .eighth)))
+        chessEngine.place(piece: pieces[0], at: Position(file: .G, rank: .eighth))
+        //And the rook will be at F8
+        XCTAssertTrue(chessEngine.pieces.contains(Piece(type: .rook, colour: .black, position: Position(file: .F, rank: .eighth))))
+        XCTAssertTrue(chessEngine.pieces.contains(Piece(type: .king, colour: .black, position: Position(file: .G, rank: .eighth))))
+    }
+    
+    func testRokadoBlackQueenSide() {
+        // Given the user has a black king at E8 and white rook at A8
+        let pieces = [Piece(type: .king, colour: .black, position: Position(file: .E, rank: .eighth)),
+                      Piece(type: .rook, colour: .black, position: Position(file: .A, rank: .eighth))]
+        let chessEngine = createSUT(pieces: pieces, turn: true)
+        // And there are no pieces that are attacking the king between the rook
+        // Then the white king can castle queenside and will have G8 as a valid move
+        XCTAssertTrue(chessEngine.validMoves(for: pieces[0]).contains(Position(file: .C, rank: .eighth)))
+        chessEngine.place(piece: pieces[0], at: Position(file: .C, rank: .eighth))
+        //And the rook will be at D8
+        XCTAssertTrue(chessEngine.pieces.contains(Piece(type: .rook, colour: .black, position: Position(file: .D, rank: .eighth))))
+        XCTAssertTrue(chessEngine.pieces.contains(Piece(type: .king, colour: .black, position: Position(file: .C, rank: .eighth))))
+    }
 }
