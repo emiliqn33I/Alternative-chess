ed24a7eedf6e030688a9498d3e9cb355a900705d
emo <emiliyan2003e@gmail.com>
2023-03-02 19:06:22 +0200

    Move chess piece and board view to separate files.

---
+++ AlternativeChess/ChessBoardView.swift
@@ -0,0 +1,158 @@
+//
+//  ChessBoardView.swift
+//  AlternativeChess
+//
+//  Created by emo on 2.03.23.
+//
+
+import Foundation
+import UIKit
+
+protocol ChessBoardViewDelegate: AnyObject {
+    func validMoves(for piece: Piece) -> [Position]
+    func didMove(piece: Piece, to position: Position)
+}
+
+class ChessBoardView: UIView {
+    static let squaresInRow = 8
+    let board = Board()
+    var pieces: [Piece] = []
+    var pieceViews: [PieceView] = []
+ 
+    weak var delegate: ChessBoardViewDelegate?
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(didTapView(_:)))
+        self.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    var currentSelectedPiece: Piece?
+    @objc func didTapView(_ sender: UITapGestureRecognizer? = nil) {
+        guard let gestureRecognizer = sender else {
+            return
+        }
+
+        if let selectedPiece = piece(at: gestureRecognizer.location(in: self)) {
+            currentSelectedPiece = selectedPiece
+        }
+        
+        if
+            let delegate = delegate,
+            let piece = currentSelectedPiece {
+            let validMoves = delegate.validMoves(for: piece)
+            drawValidMoves(validMoves: validMoves)
+        }
+        let chosenPosition = position(for: gestureRecognizer.location(in: self))
+        
+        if
+            let chosenPosition = chosenPosition,
+            let delegate = delegate,
+            let selectedPiece = currentSelectedPiece {
+            print("You have chosen to move \(selectedPiece.type) to this position \(chosenPosition). ")
+            delegate.didMove(piece: selectedPiece, to: chosenPosition)
+            
+            
+            pieceView(at: chosenPosition)?.setup(with: selectedPiece)
+        }
+    }
+    
+    var validMoveViews: [UIView] = []
+
+    func drawValidMoves(validMoves: [Position]) {
+        let side = bounds.width / CGFloat(Position.File.allCases.count)
+
+        for view in validMoveViews {
+            view.removeFromSuperview()
+        }
+        validMoveViews.removeAll()
+        
+        for position in validMoves {
+            let fileRaw = CGFloat(position.file.rawValue)
+            let y = CGFloat(PieceView.rankReversed(rank: position.rank).rawValue) * side
+            let validMoveView = UIView(frame: CGRect(x: fileRaw * side, y: y, width: side, height: side))
+            validMoveView.backgroundColor = UIColor.green.withAlphaComponent(0.5)
+            addSubview(validMoveView)
+        
+            validMoveViews.append(validMoveView)
+        }
+    }
+
+    func piece(at point: CGPoint) -> Piece? {
+        pieceViews.first { $0.frame.contains(point) }?.piece
+    }
+    
+    func pieceView(at position: Position) -> PieceView? {
+        pieceViews.first { $0.piece.position == position }
+    }
+    
+    func position(for point: CGPoint) -> Position? {
+        let selectedValidMove = validMoveViews.first { $0.frame.contains(point) }
+        var file: Position.File?
+        var rank: Position.Rank?
+        
+        if
+            let width = selectedValidMove?.frame.width,
+            let x = selectedValidMove?.frame.origin.x,
+            let y = selectedValidMove?.frame.origin.y {
+            let resultX = x / width
+            let resultY = y / width
+            
+            for aFile in Position.File.allCases {
+                if Int(resultX) == aFile.rawValue {
+                    file = aFile
+                }
+            }
+            
+            for aRank in Position.Rank.allCases {
+                if Int(resultY) == aRank.rawValue {
+                    rank = PieceView.rankReversed(rank: aRank)
+                }
+            }
+        }
+        guard
+            let file = file,
+            let rank = rank else {
+            return nil
+        }
+        return Position(file: file, rank: rank)
+    }
+
+    override func draw(_ rect: CGRect) {
+        drawBoard()
+        drawPieces(pieces: pieces)
+    }
+
+    func drawPieces(pieces: [Piece]) {
+        for piece in pieces {
+            let pieceView = PieceView(piece: piece, squareSide: frame.width / 8)
+            pieceViews.append(pieceView)
+            addSubview(pieceView)
+        }
+    }
+
+    func drawBoard() {
+        let side = bounds.width / CGFloat(ChessBoardView.squaresInRow)
+        var row = 0
+        
+        for(_, item) in board.squares.enumerated() {
+            var counter = 0
+            for square in item {
+                let path = UIBezierPath(rect: CGRect(x: CGFloat(counter) * side, y: CGFloat(row) * side, width: side, height: side))
+                if (square.color == .white) {
+                    let blackColor = UIColor.white
+                    blackColor.setStroke()
+                    UIColor.white.setFill()
+                    path.fill()
+                } else {
+                    let blackColor = UIColor.black
+                    blackColor.setStroke()
+                    UIColor.black.setFill()
+                    path.fill()
+                }
+                counter += 1
+            }
+            row += 1
+        }
+    }
+}
---
+++ AlternativeChess/PieceView.swift
@@ -0,0 +1,84 @@
+//
+//  PieceView.swift
+//  AlternativeChess
+//
+//  Created by emo on 2.03.23.
+//
+
+import Foundation
+import UIKit
+
+class PieceView: UIView {
+    var piece: Piece
+    private let squareSide: CGFloat
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    init(piece: Piece, squareSide: CGFloat) {
+        self.piece = piece
+        self.squareSide = squareSide
+        let frame = PieceView.rect(for: piece.position, squareSide: squareSide)
+         
+        super.init(frame: frame)
+        let imageView = imageView(for: piece)
+        imageView.frame = CGRect(x: 0, y: 0, width: frame.width, height: frame.height)
+        addSubview(imageView)
+    }
+    
+    func setup(with piece: Piece) {
+        let frame = PieceView.rect(for: piece.position, squareSide: squareSide)
+        self.frame = frame
+        self.piece = piece
+
+    }
+
+    private func imageView(for piece: Piece) -> UIImageView {
+        var imageName: String = ""
+        switch piece.type {
+        case .pawn:
+            imageName = piece.colour == .white ? "w_pawn" : "b_pawn"
+        case .rook:
+            imageName = piece.colour == .white ? "w_rook" : "b_rook"
+        case .bishop:
+            imageName = piece.colour == .white ? "w_bishop" : "b_bishop"
+        case .knight:
+            imageName = piece.colour == .white ? "w_knight" : "b_knight"
+        case .queen:
+            imageName = piece.colour == .white ? "w_queen" : "b_queen"
+        case .king:
+            imageName = piece.colour == .white ? "w_king" : "b_king"
+        }
+        let imageView = UIImageView(image: UIImage(named: imageName))
+        return imageView
+    }
+    
+    private static func rect(for position: Position, squareSide: CGFloat) -> CGRect {
+        let fileRaw = CGFloat(position.file.rawValue)
+        let y = CGFloat(PieceView.rankReversed(rank: position.rank).rawValue) * squareSide
+        return CGRect(x: fileRaw * squareSide, y: y, width: squareSide, height: squareSide)
+    }
+
+     static func rankReversed(rank: Position.Rank) -> Position.Rank {
+        switch rank {
+        case .first:
+            return .eighth
+        case .second:
+            return .seventh
+        case .third:
+            return .sixth
+        case .fourth:
+            return .fifth
+        case .fifth:
+            return .fourth
+        case .sixth:
+            return .third
+        case .seventh:
+            return .second
+        case .eighth:
+            return .first
+        }
+    }
+}
+
