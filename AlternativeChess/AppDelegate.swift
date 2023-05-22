a108a4a77be84164ad0034d82d54e54d55c785b1
emo <emiliyan2003e@gmail.com>
2023-05-22 19:19:28 +0300

    Configurate parse client.

--- AlternativeChess/AppDelegate.swift
+++ AlternativeChess/AppDelegate.swift
@@ -6,12 +6,21 @@
 //
 
 import UIKit
+import Parse
+import ParseLiveQuery
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        //Override point for customization after application launch.
+        let configuration = ParseClientConfiguration {
+            $0.applicationId = "6HuvtKxwWb5dHCxiLpZkpW5s5mCLgq3LxEbK2jgh"
+            $0.clientKey = "dOMIWcIwfuxi0i6fka4Nk8gBfvZUxyGsA6IqcZt0"
+            $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: configuration)
+        
         return true
     }
 
